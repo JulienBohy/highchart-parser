419f7c2a7b86f17e8f1f13c20826bcb2c0675737
Bohy Julien <j.bohy@sbim.be>
2021-03-25 13:39:07 +0100

    Color change

--- src/Const.php
+++ src/Const.php
@@ -91,7 +91,7 @@ namespace Highcharts\Constants;
 
     class colors
     {
-        const DEFAULTCOLORS = ['#e44718', '#009FB1', '#00B16B', '#800080', '#FF8040', '#808080', '#6960EC', '#FFFC9F', '#306754', '#8C001A', '#FAAFBE', '#4B0082', '#000000', '#FF7F50', '#E3319D'];
+        const DEFAULTCOLORS = ['#e44718', '#009FB1', '#00B16B', '#800080', '#FF8040', '#808080', '#6960EC', '#EFCB1B', '#306754', '#8C001A', '#FAAFBE', '#4B0082', '#000000', '#FF7F50', '#E3319D'];
         const TARGETCOLORS = ['green', 'darkorange', 'red'];
         const LINESCOLORS = ['gray' => '#b3b3b2'];
     }
