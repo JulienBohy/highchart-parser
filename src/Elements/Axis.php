4ffdc4d2d57aadd663f34c1f8b934ce26384c50d
Bohy Julien <j.bohy@sbim.be>
2021-07-16 15:33:44 +0200

    Debug

--- src/Elements/Axis.php
+++ src/Elements/Axis.php
@@ -1,21 +1,13 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 namespace Highcharts\Elements;
 
 use JsonSerializable;
 
 
-/**
- * Description of XAxis.
- *
- * @author BOJU
- */
+
 class Axis implements JsonSerializable
 {
     private $title;
